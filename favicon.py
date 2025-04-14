5a48d74f99aed8adee6c0846692a3a0289ac749e
arnelkovacevic <arnel.kovacevic@icloud.com>
2025-04-14 21:29:32 +0200

    Update favicon.py

--- favicon.py
+++ favicon.py
@@ -5,9 +5,9 @@ def set_favicon():
         """
         <link rel="icon" href="https://i.ibb.co/nqFLS5Kh/GMR.png" type="image/png"/>
         <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-status-bar-style" content="black">
         <meta name="apple-mobile-web-app-title" content="GMR">
-        <link rel="apple-touch-icon" href="https://i.ibb.co/nqFLS5Kh/GMR.png">
+        <link rel="apple-touch-icon" href="https://i.ibb.co/gzhq2Vm/gmr-180.png">
         """,
         unsafe_allow_html=True
     )
-    
