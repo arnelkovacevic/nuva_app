61633aeb15cbc5e2e5abbfb8cf2b37764113c3c4
arnelkovacevic <arnel.kovacevic@icloud.com>
2025-04-14 20:36:31 +0200

    Update logo.py

--- logo.py
+++ logo.py
@@ -4,11 +4,9 @@ def show_logo():
     st.markdown(
         """
         <div style='display: flex; align-items: center; justify-content: flex-start; margin-bottom: 20px;'>
-            <img src='https://i.imgur.com/a/ItjW4Vh.png'width='100'/>
+            <img src='https://i.ibb.co/nqFLS5Kh/GMR.png' width='100'/>
             <h2 style='margin-left: 20px;'>GMR Inventario</h2>
         </div>
         """,
         unsafe_allow_html=True
     )
-    
-    
