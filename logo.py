04cbbfee2918ad546fd970ee6a4df8547e28a23c
arnelkovacevic <arnel.kovacevic@icloud.com>
2025-04-14 20:02:25 +0200

    Update logo.py
    Added logo gmr

--- logo.py
+++ logo.py
@@ -4,11 +4,9 @@ def show_logo():
     st.markdown(
         """
         <div style='display: flex; align-items: center; justify-content: flex-start; margin-bottom: 20px;'>
-            <img src='https://www.dropbox.com/scl/fi/ylush0jc19rum18nqb8rs/apple-touch-icon-180x180.png?rlkey=w8vsuhanee8udezq5aiziijgp&st=5j2wwgo1&dl=1' width='100'/>
+            <img src='https://i.imgur.com/ExdKOOz.png' width='100'/>
             <h2 style='margin-left: 20px;'>GMR Inventario</h2>
         </div>
         """,
         unsafe_allow_html=True
     )
-    
-    
