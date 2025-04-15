9e7324f87bbc9a1b000af8426fd260fff400c27a
arnelkovacevic <arnel.kovacevic@icloud.com>
2025-04-16 00:35:56 +0200

    Update logo.py

--- logo.py
+++ logo.py
@@ -3,7 +3,7 @@ import streamlit as st
 def show_logo():
     st.markdown(
         """
-        <div style='margin-top: -10px; margin-left: 45px;'>
+        <div style='margin-top: -10px; margin-left: 5px;'>
             <img src='https://i.ibb.co/cK1skNcK/IMG-6318.png' width='18'/>
         </div>
         <div style='display: flex; align-items: center; justify-content: flex-start; margin-bottom: 10px;'>
