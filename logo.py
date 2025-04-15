77b0ddddbefd53dd9c6bcd5286ce692a61d277c9
arnelkovacevic <arnel.kovacevic@icloud.com>
2025-04-16 00:17:18 +0200

    Update logo.py
    Resized Logos

--- logo.py
+++ logo.py
@@ -4,11 +4,11 @@ def show_logo():
     st.markdown(
         """
         <div style='display: flex; align-items: center; justify-content: flex-start; margin-bottom: 10px;'>
-            <img src='https://i.ibb.co/nqFLS5Kh/GMR.png' width='100'/>
+            <img src='https://i.ibb.co/nqFLS5Kh/GMR.png' width='80'/>
             <h2 style='margin-left: 20px;'>GMR Inventario</h2>
         </div>
         <div style='margin-top: -10px; margin-left: 5px;'>
-            <img src='https://i.ibb.co/cK1skNcK/IMG-6318.png' width='50'/>
+            <img src='https://i.ibb.co/cK1skNcK/IMG-6318.png' width='25'/>
         </div>
         """,
         unsafe_allow_html=True
