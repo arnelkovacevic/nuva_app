9d0a39747a0fca0519b3c94bb899f760e3d02236
arnelkovacevic <arnel.kovacevic@icloud.com>
2025-04-16 00:28:22 +0200

    Update logo.py
    Logo up x

--- logo.py
+++ logo.py
@@ -3,13 +3,13 @@ import streamlit as st
 def show_logo():
     st.markdown(
         """
+        <div style='margin-top: -10px; margin-left: 45px;'>
+            <img src='https://i.ibb.co/cK1skNcK/IMG-6318.png' width='18'/>
+        </div>
         <div style='display: flex; align-items: center; justify-content: flex-start; margin-bottom: 10px;'>
-            <img src='https://i.ibb.co/nqFLS5Kh/GMR.png' width='80'/>
-            <h2 style='margin-left: 20px;'>GMR Inventario</h2>
-        </div>
-        <div style='margin-top: -10px; margin-left: 5px;'>
-            <img src='https://i.ibb.co/cK1skNcK/IMG-6318.png' width='25'/>
-        </div>
+            <img src='https://i.ibb.co/nqFLS5Kh/GMR.png' width='75'/>
+            <h2 style='margin-left: 20px;'>gXLS - Reader</h2>
+        
         """,
         unsafe_allow_html=True
     )
