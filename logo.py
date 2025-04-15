fa669721eceb6e495f84f45319826da1abc12e12
arnelkovacevic <arnel.kovacevic@icloud.com>
2025-04-15 23:58:44 +0200

    Update logo.py
    Ispravka logo

--- logo.py
+++ logo.py
@@ -3,12 +3,9 @@ import streamlit as st
 def show_logo():
     st.markdown(
         """
-        <div style='display: flex; align-items: center; justify-content: flex-start; margin-bottom: 10px;'>
+        <div style='display: flex; align-items: center; justify-content: flex-start; margin-bottom: 20px;'>
             <img src='https://i.ibb.co/nqFLS5Kh/GMR.png' width='100'/>
             <h2 style='margin-left: 20px;'>GMR Inventario</h2>
-        </div>
-        <div style='margin-top: -10px; margin-left: 5px;'>
-            <img src='https://i.ibb.co/nqFLS5Kh/GMR.png' width='50'/>
         </div>
         """,
         unsafe_allow_html=True
