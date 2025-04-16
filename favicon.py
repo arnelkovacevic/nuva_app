140c9fbeaea9ce0f2b43f556718845b996bcec52
arnelkovacevic <arnel.kovacevic@icloud.com>
2025-04-16 11:03:36 +0200

    Update favicon.py

--- favicon.py
+++ favicon.py
@@ -3,11 +3,11 @@ import streamlit as st
 def set_favicon():
     st.markdown(
         """
-        <link rel="icon" href="https://i.ibb.co/cK1skNcK/IMG-6318.png" type="image/png"/>
-        <meta name="apple-mobile-web-app-capable" content="yes">
-        <meta name="apple-mobile-web-app-status-bar-style" content="black">
-        <meta name="apple-mobile-web-app-title" content="XLS">
+        <link rel="icon" href="https://i.ibb.co/cK1skNcK/IMG-6318.png" type="image/png">
         <link rel="apple-touch-icon" href="https://i.ibb.co/WpN739Cm/apple-touch-icon-180x180.png">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+        <meta name="apple-mobile-web-app-title" content="gXLS">
         """,
         unsafe_allow_html=True
     )
