b259b78322e0a4f37fc24655b548e9e642b275e2
arnelkovacevic <arnel.kovacevic@icloud.com>
2025-05-06 00:03:22 +0200

    Update logo.py orizon logo 50%

--- logo.py
+++ logo.py
@@ -6,10 +6,10 @@ def show_logo():
         <div style='margin-top: -10px; margin-left: 55px;'>
             <img src='https://i.ibb.co/cK1skNcK/IMG-6318.png' width='20'/>
         </div>
-        <div style='display: flex; align-items: center; justify-content: flex-start; margin-bottom: 10px;'>
+        <div style='display: flex; align-items: center; justify-content: flex-start; margin-bottom: 15px;'>
             <a href='https://gmr-inventario.app/login' target='_blank'>
                 <img src='https://i.ibb.co/nqFLS5Kh/GMR.png' width='75'/>
-                <img src='https://i.ibb.co/MkdYPmgJ/ba515e32-8e6a-4a78-8491-0567df6debb4.png' width='75'/>
+                <img src='https://i.ibb.co/MkdYPmgJ/ba515e32-8e6a-4a78-8491-0567df6debb4.png' width='50'/>
             </a>
             <h2 style='margin-left: 20px;'>gXLS - Reader</h2>
         </div>
